83f7315684785cc54d4b3456eec9453355669681
Marshall Houston <marshall.houston@gmail.com>
2017-10-22 19:42:06 -0600

    add some notes re factory_bot testing

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,3 +1,17 @@
-FactoryGirl.define do
-  #<factories for each model go here>
+FactoryBot.define do
+  factory :idea do
+    name "Jobs"
+    description "find a new job"
+    admin false
+  end
+
+  
+  # This will use the User class (Admin would have been guessed)
+ # factory :admin, class: User do
+ #   first_name "Admin"
+ #   last_name  "User"
+ #   admin      true
+ # end
+
+
 end
