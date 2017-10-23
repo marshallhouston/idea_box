be1d8b448a53bc21e55acc3e2d002e8dc010d1b5
Marshall Houston <marshall.houston@gmail.com>
2017-10-22 21:07:54 -0600

    add new/create/show actions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      # add message for success?
+      redirect_to user_path(user)
+    else
+      # add message for failure
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
