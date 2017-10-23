a7bc811bb95a755254eccde2d96ba556afaf779d
Marshall Houston <marshall.houston@gmail.com>
2017-10-23 13:52:19 -0600

    add passing test for user visits single idea page

---
+++ spec/features/ideas/user_can_see_an_idea_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "user can visit a single idea page" do
+
+  before :each do
+    @user = create(:user)
+    @idea1 = Idea.create!(name: "idea1", description: "read", user: @user, category: build(:category))
+    @idea2 = Idea.create!(name: "idea2", description: "learn", user: @user, category: build(:category))
+  end
+
+  it "user can click on a single idea see the idea show page" do
+    visit user_ideas_path(@user)
+
+    click_on "idea1"
+
+    expect(current_path).to eq(user_idea_path(@user, @idea1))
+    expect(page).to have_content(@idea1.name)
+    expect(page).to have_content(@idea1.description)
+    expect(page).to_not have_content(@idea2.name)
+    expect(page).to_not have_content(@idea2.description)
+  end
+end
