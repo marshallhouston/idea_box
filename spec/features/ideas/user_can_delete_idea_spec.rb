ded814f747bf381a40f8629afb2cf6a76df1b28e
Marshall Houston <marshall.houston@gmail.com>
2017-10-23 14:03:19 -0600

    add passing test for deleting idea

---
+++ spec/features/ideas/user_can_delete_idea_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "user can delete an idea" do
+
+  before :each do
+    @user = create(:user)
+    @idea1 = Idea.create!(name: "idea1", description: "read", user: @user, category: build(:category))
+  end
+
+  it "clicks delete and the idea is removed" do
+    visit user_idea_path(@idea1.user, @idea1)
+
+    click_on 'Delete this idea'
+
+    expect(current_path).to eq(user_ideas_path(@user))
+    expect(page).to have_content(@user.name)
+    expect(page).to_not have_content(@idea1.name)
+    expect(page).to_not have_content(@idea1.description)
+  end
+end
