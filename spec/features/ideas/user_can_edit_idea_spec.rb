b673ff945d23260f6702982f9d1709120d668046
Marshall Houston <marshall.houston@gmail.com>
2017-10-24 15:03:07 -0600

    remove log in from setup in test

--- spec/features/ideas/user_can_edit_idea_spec.rb
+++ spec/features/ideas/user_can_edit_idea_spec.rb
@@ -1,24 +1,18 @@
 require 'rails_helper'
 
 describe "user edits an existing idea" do
-
-  before :each do
-    @user = create(:user)
-    @category = Category.create!(name: "hobbies", description: "ideas for hobbies")
-    @idea1 = Idea.create!(name: "idea1", description: "read", user: @user, category: build(:category))
-    visit root_path
-    click_on 'Log In'
-    fill_in 'session[email]', with: @user.email
-    fill_in 'session[password]', with: @user.password
-    click_on 'Log In'
-  end
-
   it "changes information for an idea and it updates that information" do
-    visit user_idea_path(@idea1.user, @idea1)
+    user = create(:user)
+    Category.create!(name: "hobbies", description: "ideas for hobbies")
+    idea1 = Idea.create!(name: "idea1", description: "read", user: user, category: build(:category))
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_idea_path(idea1.user, idea1)
 
     click_on "Edit this idea"
 
-    expect(current_path).to eq(edit_user_idea_path(@idea1.user, @idea1))
+    expect(current_path).to eq(edit_user_idea_path(idea1.user, idea1))
 
     fill_in "idea[name]", with: "LEARN!"
     fill_in "idea[description]", with: "new topics to learn"
@@ -26,7 +20,7 @@ describe "user edits an existing idea" do
 
     click_on "Update Idea"
 
-    expect(current_path).to eq(user_idea_path(@idea1.user, @idea1))
+    expect(current_path).to eq(user_idea_path(idea1.user, idea1))
     expect(page).to have_content("LEARN!")
     expect(page).to have_content("new topics to learn")
     expect(page).to have_content("hobbies")
