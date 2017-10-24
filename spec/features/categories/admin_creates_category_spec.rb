5e49c0d9eb7a1963fe6e9e871ebecdc79c1aa8b5
Marshall Houston <marshall.houston@gmail.com>
2017-10-24 15:49:05 -0600

    add test for admin categories index

--- spec/features/categories/admin_creates_category_spec.rb
+++ spec/features/categories/admin_creates_category_spec.rb
@@ -1,9 +1,40 @@
 require 'rails_helper'
 
-describe "When I visit admin categories create" do
+describe "When I visit admin new category" do
   describe "as an admin" do
-    scenario "I can create a new category" do
+    scenario "I see a new form" do
+      admin_user = User.create!(name: "admin",
+                                email: "admin@admin.com",
+                                password: "pass",
+                                role: 1)
 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
+
+      visit new_admin_category_path
+
+      expect(page).to have_content("Create A New Category")
+
+      fill_in "category[name]", with: "cats"
+      fill_in "category[description]", with: "ideas about cats"
+
+      click_on "Create Category"
+
+      expect(current_path).to eq(admin_categories_path)
+      expect(page).to have_content("cats")
+    end
+  end
+
+  describe "as a default user" do
+    scenario "I see a 404 error" do
+      default_user = User.create!(name: "default",
+                                  email: "def@def.com",
+                                  password: "pass")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user)
+
+      visit new_admin_category_path
+
+      expect(status_code).to eq(404)
     end
   end
 end
--- spec/features/categories/admin_sees_all_categories_spec.rb
+++ spec/features/categories/admin_sees_all_categories_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
-describe "When I visit admin new category" do
+describe "When I visit admin categories index" do
   describe "as an admin" do
-    scenario "I see a new form" do
+    scenario "I can see all new categories" do
+      category1 = Category.create!(name: "cats", description: "cats1")
+      category2 = Category.create!(name: "dogs", description: "dogs1")
+
       admin_user = User.create!(name: "admin",
                                 email: "admin@admin.com",
                                 password: "pass",
@@ -10,17 +13,10 @@ describe "When I visit admin new category" do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
 
-      visit new_admin_category_path
+      visit admin_categories_path
 
-      expect(page).to have_content("Create A New Category")
-
-      fill_in "category[name]", with: "cats"
-      fill_in "category[description]", with: "ideas about cats"
-
-      click_on "Create Category"
-
-      expect(current_path).to eq(admin_categories_path)
-      expect(page).to have_content("cats")
+      expect(page).to have_content(category1.name)
+      expect(page).to have_content(category2.name)
     end
   end
 
