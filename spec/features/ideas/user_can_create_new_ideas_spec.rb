083b3ac41b5c33cf8e52395ed151604ef40a3cc4
Marshall Houston <marshall.houston@gmail.com>
2017-10-24 14:59:47 -0600

    remove log in from set up method

--- spec/features/ideas/user_can_create_new_ideas_spec.rb
+++ spec/features/ideas/user_can_create_new_ideas_spec.rb
@@ -1,21 +1,15 @@
 require 'rails_helper'
 
 describe "user creates new idea" do
-  before :each do
-    @user = create(:user)
-    @category = create(:category)
-
-    visit root_path
-    click_on 'Log In'
-    fill_in 'session[email]', with: @user.email
-    fill_in 'session[password]', with: @user.password
-    click_on 'Log In'
-  end
-
   scenario "creates new idea" do
+    user = create(:user)
+    create(:category)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
     click_on('Create a new idea')
 
-    expect(current_path).to eq(new_user_idea_path(@user))
+    expect(current_path).to eq(new_user_idea_path(user))
 
     fill_in 'idea[name]', with: "read"
     fill_in 'idea[description]', with: "novels or magazines"
@@ -23,7 +17,7 @@ describe "user creates new idea" do
 
     click_on 'Create Idea'
 
-    expect(current_path).to eq(user_idea_path(@user, Idea.last))
+    expect(current_path).to eq(user_idea_path(user, Idea.last))
 
     expect(page).to have_content("read")
     expect(page).to have_content("novels or magazines")
