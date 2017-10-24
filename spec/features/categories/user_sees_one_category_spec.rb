3e3ff79596cdf4075d738d00d124b0a6ab9945bd
Marshall Houston <marshall.houston@gmail.com>
2017-10-23 21:47:14 -0600

    test for ideas in a category WIP

--- spec/features/categories/user_sees_one_category_spec.rb
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 describe "user can view a single category" do
-  before :each do
-    @category1 = Category.create!(name: 'cats', description: 'cat things')
-    @category2 = Category.create(name: 'fish', description: 'trout')
+  describe "that doesn't have any ideas in the category" do
+    before :each do
+      @category1 = Category.create!(name: 'cats', description: 'cat things')
+      @category2 = Category.create(name: 'fish', description: 'trout')
+    end
+
+    scenario "user clicks on a category and it shows the category" do
+      visit categories_path
+
+      click_on "cats"
+
+      expect(current_path).to eq(category_path(@category1))
+      expect(page).to have_content(@category1.name)
+      expect(page).to have_content(@category1.description)
+      expect(page).to_not have_content(@category2.name)
+    end
   end
 
-  scenario "user clicks on a category and it shows the category" do
-    visit categories_path
+  xdescribe "that has ideas" do
 
-    click_on "cats"
+    before :each do
+      #create a user
+      #create a category with 2 ideas
+      #log that user in
+    end
 
-    expect(current_path).to eq(category_path(@category1))
-    expect(page).to have_content(@category1.name)
-    expect(page).to have_content(@category1.description)
-    expect(page).to_not have_content(@category2.name)
+    it "shows the ideas for the category" do
+      #
+    end
   end
 end
