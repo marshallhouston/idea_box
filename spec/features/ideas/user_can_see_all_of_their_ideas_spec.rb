0ecb44aa81fded30759067ac629c14bb205cf7fb
Marshall Houston <marshall.houston@gmail.com>
2017-10-24 15:04:46 -0600

    remove login from test setup

--- spec/features/ideas/user_can_see_all_of_their_ideas_spec.rb
+++ spec/features/ideas/user_can_see_all_of_their_ideas_spec.rb
@@ -1,24 +1,19 @@
 require 'rails_helper'
 
 describe "user sees a list of ideas" do
-  before :each do
-    #i'd like to be able to have multiple diferent ideas created from a factory
-    @user = create(:user)
-    @idea1 = Idea.create!(name: "idea1", description: "read", user: @user, category: build(:category))
-    @idea2 = Idea.create!(name: "idea2", description: "learn", user: @user, category: build(:category))
-    visit root_path
-    click_on 'Log In'
-    fill_in 'session[email]', with: @user.email
-    fill_in 'session[password]', with: @user.password
-    click_on 'Log In'
-  end
-
   scenario "user has multiple ideas on a page" do
-    visit user_ideas_path(@user)
+    #i'd like to be able to have multiple diferent ideas created from a factory
+    user = create(:user)
+    idea1 = Idea.create!(name: "idea1", description: "read", user: user, category: build(:category))
+    idea2 = Idea.create!(name: "idea2", description: "learn", user: user, category: build(:category))
 
-    expect(page).to have_content(@idea1.name)
-    expect(page).to have_content(@idea1.description)
-    expect(page).to have_content(@idea2.name)
-    expect(page).to have_content(@idea2.description)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_ideas_path(user)
+
+    expect(page).to have_content(idea1.name)
+    expect(page).to have_content(idea1.description)
+    expect(page).to have_content(idea2.name)
+    expect(page).to have_content(idea2.description)
   end
 end
