b52843bccb101d62b119e3451b0607ac5edc2715
Marshall Houston <marshall.houston@gmail.com>
2017-10-24 14:33:10 -0600

    run migration to add role column to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024195855) do
+ActiveRecord::Schema.define(version: 20171024203123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20171024195855) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "ideas", "categories"
