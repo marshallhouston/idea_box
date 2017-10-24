716a19a140a00655389077ad10bd06c97dfaa17b
Marshall Houston <marshall.houston@gmail.com>
2017-10-24 16:23:24 -0600

    add test for admin deleting category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -32,6 +32,13 @@ class Admin::CategoriesController < Admin::BaseController
     end
   end
 
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+
+    redirect_to admin_categories_path
+  end
+
   private
 
   def category_params
--- spec/features/categories/admin_deletes_category_spec.rb
+++ spec/features/categories/admin_deletes_category_spec.rb
@@ -1,3 +1,45 @@
 require 'rails_helper'
 
-describe "When I "
+describe "When I try to delete a category" do
+  describe "as an admin" do
+    scenario "I can delete the category" do
+      Category.create!(name: "cats", description: "cats1")
+      category2 = Category.create!(name: "dogs", description: "dogs1")
+      admin_user = User.create!(name: "admin",
+                                email: "admin@admin.com",
+                                password: "pass",
+                                role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
+
+      visit admin_categories_path
+
+      within ".categories" do
+        click_link("Delete this category", :match => :first)
+      end
+
+      expect(current_path).to eq(admin_categories_path)
+
+      expect(page).to have_content(category2.name)
+      expect(page).to_not have_content("cats")
+    end
+  end
+
+  describe "as a default user" do
+    scenario "I see a 404 error" do
+      Category.create!(name: "cats", description: "cats1")
+      default_user = User.create!(name: "default",
+                                  email: "def@def.com",
+                                  password: "pass")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user)
+
+      #how to test this functionality???????????????????????????????????
+      # a user won't be able to see the admin_categories path
+      visit admin_categories_path
+
+      expect(status_code).to eq(404)
+    end
+  end
+
+end
