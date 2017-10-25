fb231eea6ae6a4382ea55d338ce1e0d8a7daa381
Marshall Houston <marshall.houston@gmail.com>
2017-10-24 22:08:04 -0600

    add before_actions, replace @user with current_user

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,59 +1,54 @@
 class IdeasController < ApplicationController
 
   before_action :check_logged_in
+  before_action :find_idea, only: [:show, :edit, :update, :destroy]
+  before_action :load_images_and_categories, only: [:new, :edit]
 
   def index
-    @ideas = Idea.where(user_id: params[:user_id])
-    @user = User.find(params[:user_id])
+    @ideas = current_user.ideas
   end
 
   def new
-    @user = User.find(params[:user_id])
-    @idea = @user.ideas.new
-    @categories = Category.all
+    @idea = current_user.ideas.new
   end
 
   def show
-    @idea = Idea.find(params[:id])
-    @user = @idea.user
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @categories = Category.all
-    @idea = @user.ideas.new(idea_params)
+    @idea = current_user.ideas.new(idea_params)
     if @idea.save
-      
-      redirect_to user_idea_path(@user, @idea)
+      redirect_to user_idea_path(current_user, @idea)
     else
       render :new
     end
   end
 
   def edit
-    @idea = Idea.find(params[:id])
-    @categories = Category.all
-    @user = @idea.user
   end
 
   def update
-    idea = Idea.find(params[:id])
-    idea.update(idea_params)
-    redirect_to user_idea_path(idea.user, idea)
+    @idea.update(idea_params)
+    redirect_to user_idea_path(current_user, @idea)
   end
 
   def destroy
-    idea = Idea.find(params[:id])
-    user = idea.user
-    idea.destroy
-
-    redirect_to user_ideas_path(user)
+    @idea.destroy
+    redirect_to user_ideas_path(current_user)
   end
 
   private
 
   def idea_params
-    params.require(:idea).permit(:name, :description, :category_id)
+    params.require(:idea).permit(:name, :description, :category_id, image_ids:[])
   end
 
+  def find_idea
+    @idea = Idea.find(params[:id])
+  end
+
+  def load_images_and_categories
+    @categories = Category.all
+    @images = Image.all
+  end
 end
