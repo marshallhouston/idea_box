9f36d5a56c0c0339d3b6c5865319b826321d1af9
Marshall Houston <marshall.houston@gmail.com>
2017-10-28 20:03:06 -0600

    add links to ideas

--- spec/features/categories/user_sees_one_category_spec.rb
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 describe "user can view a single category" do
   describe "that doesn't have any ideas in the category" do
-    before :each do
-      @category1 = Category.create!(name: 'cats', description: 'cat things')
-      @category2 = Category.create(name: 'fish', description: 'trout')
-    end
-
     scenario "user clicks on a category and it shows the category" do
+      category1 = Category.create!(name: 'cats', description: 'cat things')
+      category2 = Category.create(name: 'fish', description: 'trout')
+
       visit categories_path
 
       click_on "cats"
 
-      expect(current_path).to eq(category_path(@category1))
-      expect(page).to have_content(@category1.name)
-      expect(page).to have_content(@category1.description)
-      expect(page).to_not have_content(@category2.name)
+      expect(current_path).to eq(category_path(category1))
+      expect(page).to have_content(category1.name)
+      expect(page).to have_content(category1.description)
+      expect(page).to_not have_content(category2.name)
     end
   end
 
