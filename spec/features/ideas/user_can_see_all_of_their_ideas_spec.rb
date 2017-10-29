4849f831477a6b7cf432fd6d555a9499f081d0e7
Marshall Houston <marshall.houston@gmail.com>
2017-10-29 15:18:07 -0600

    add link for creating new idea on idea index

--- spec/features/ideas/user_can_see_all_of_their_ideas_spec.rb
+++ spec/features/ideas/user_can_see_all_of_their_ideas_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe "user sees a list of ideas" do
   scenario "user has multiple ideas on a page" do
-    #i'd like to be able to have multiple diferent ideas created from a factory
     user = create(:user)
     idea1 = Idea.create!(name: "idea1", description: "read", user: user, category: build(:category))
     idea2 = Idea.create!(name: "idea2", description: "learn", user: user, category: build(:category))
@@ -11,6 +10,7 @@ describe "user sees a list of ideas" do
 
     visit user_ideas_path(user)
 
+    expect(page).to have_link("Create a new idea")
     expect(page).to have_content(idea1.name)
     expect(page).to have_content(idea1.description)
     expect(page).to have_content(idea2.name)
